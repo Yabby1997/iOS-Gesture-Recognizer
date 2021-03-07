47802c177e6223799d3a72b2cf0d75893959130d
seunghun yang <yabby1997@gmail.com>
2021-03-07 15:28:16 +0900

    IBAction, Target-action, Delegate방식으로 Gesture사용해보기

--- gesture/ViewController.swift
+++ gesture/ViewController.swift
@@ -7,13 +7,24 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIGestureRecognizerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let tapGesture: UITapGestureRecognizer = UITapGestureRecognizer()
+        tapGesture.delegate = self
+        self.view.addGestureRecognizer(tapGesture)
+    }
+    
+    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldReceive touch: UITouch) -> Bool {
+        self.view.endEditing(true)
+        return true
     }
 
-
+    @IBAction func tapView(_ sender: Any) {
+        self.view.endEditing(true)
+    }
+    
 }
 
